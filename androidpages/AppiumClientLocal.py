f12a6e5b811685ff9b8f77e53fcbe6530031d76c
Ubuntu <ubuntu@ip-172-31-1-226.us-east-2.compute.internal>
2019-11-26 11:27:30 +0000

    first

--- androidpages/AppiumClientLocal.py
+++ androidpages/AppiumClientLocal.py
@@ -1,7 +1,7 @@
 class AppiumClientLocal:
     def __init__(self):
         self._url = "http://82.13.42.180:4723/wd/hub"
-        self.name = "uiautomator2"
+        self.name = "UiAutomator2"
         self.appium_path = "/usr/local/lib/node_modules/appium/build/lib/main.js"
         self.node_path = "/usr/local/bin/node"
         self.args = ['--address', '82.13.42.180', '--session-override', 'true']
