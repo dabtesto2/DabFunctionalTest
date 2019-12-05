7fc42ffb4d54222cd3576b09dbf3ea7960087b78
dabtesto2 <dabtesto2@gmail.com>
2019-12-05 20:14:19 +0000

    Updated code

--- androidpages/AndroidDevicePool.py
+++ androidpages/AndroidDevicePool.py
@@ -3,9 +3,9 @@ class AndroidDevicePool():
     def __init__(self, device_profile):
         self.device_profile = device_profile
         self._device = {
-            "dev1": {"udid": "ce0417145b24bca70d", "profile": "NDS"},
-            "dev2": {"udid": "2adbc704fc0d7ece", "profile": "PC"},
-            "dev3": {"udid": "ce0417145b24bca70d", "profile": "OA"}
+            "dev1": {"udid": "ce0417145b24bca70d", "profile": "NDS", "msisdn": "447516903691", "imsi":"234106663078773"},
+            "dev2": {"udid": "2adbc704fc0d7ece", "profile": "PC", "msisdn":"447725629466", "imsi":"234106663078775"},
+            "dev3": {"udid": "ce0417145b24bca70d", "profile": "OA",  "msisdn": "447516903691", "imsi":"234106663078773"}
         }
         self.platform = "Android"
         self.fastReset = True
@@ -14,10 +14,20 @@ class AndroidDevicePool():
         self.startuppackage = "com.android.settings"
         self.browsernamechrome = "chrome"
 
-
     def get_android_device_id(self):
         for device in self._device.values():
             if device['profile'] in self.device_profile:
                 return device['udid']
         raise Exception(f'Requested device profile {self.device_profile} not Found')
 
+    def get_android_device_msisdn(self):
+        for device in self._device.values():
+            if device['profile'] in self.device_profile:
+                return device['msisdn']
+        raise Exception(f'Requested device profile {self.device_profile} not Found')
+
+    def get_android_device_imsi(self):
+        for device in self._device.values():
+            if device['profile'] in self.device_profile:
+                return device['imsi']
+        raise Exception(f'Requested device profile {self.device_profile} not Found')
