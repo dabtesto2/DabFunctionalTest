539316b9c928aab9b5a796fcf43c311c26531347
purush <purushflam@gmail.com>
2019-12-09 12:06:12 +0000

    updated

--- features/steps/chrome_test.py
+++ features/steps/chrome_test.py
@@ -17,7 +17,8 @@ def step_impl(context, browser, url):
 def step_impl(context, title, no_links):
     context.chrome_page_obj.dismiss_message_box_if_any()
     context.chrome_page_obj.check_document_ready_state(title)
-    print(context.chrome_page_obj.find_if_any_error_in_chrome_page)
+    var = context.chrome_page_obj.find_if_any_error_in_chrome_page
+    print(str(var))
     title = title.lower()
     assert_that(context.chrome_page_obj.get_web_page_source(), contains_string(title), raises(ValueError, title))
     context.chrome_page_obj.get_links_from_page()
