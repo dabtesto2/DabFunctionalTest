b7062f201de96af4c8fb4f9055bea776682e76c4
purush <purushflam@gmail.com>
2019-12-09 22:45:41 +0000

    updated

--- features/steps/chrome_test.py
+++ features/steps/chrome_test.py
@@ -1,6 +1,7 @@
 from behave import *
 from chrome.ChromeDriverPage import ChromeDriverPage
 from hamcrest import *
+import re
 
 
 @then(u'Open "{browser}" browser and get url "{url}"')
@@ -17,9 +18,11 @@ def step_impl(context, browser, url):
 def step_impl(context, title, no_links):
     context.chrome_page_obj.dismiss_message_box_if_any()
     context.chrome_page_obj.check_document_ready_state(title)
-    assert_that(str(context.chrome_page_obj.get_web_page_source()),
-                not_(contains_string("ERR_TIMED_OUT".lower())),
-                raises(Exception, " Page Timeout Occured User Plane Error"))
+    value = re.findall(r"err\_timed\_out", context.chrome_page_obj.get_web_page_source())
+    if len(value) > 0:
+        assert_that(str(value[0]),
+                    not_(contains_string("ERR_TIMED_OUT".lower())),
+                    raises(Exception, " Page Timeout Occured User Plane Error"))
     title = title.lower()
     assert_that(context.chrome_page_obj.get_web_page_source(), contains_string(title), raises(ValueError, title))
     context.chrome_page_obj.get_links_from_page()
