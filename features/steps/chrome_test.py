a88fecd9b8ebb8bf5ff24be8a637ed8987ab94d8
purush <purushflam@gmail.com>
2019-12-10 05:11:33 +0000

    updated

--- features/steps/chrome_test.py
+++ features/steps/chrome_test.py
@@ -18,7 +18,7 @@ def step_impl(context, browser, url):
 def step_impl(context, title, no_links):
     context.chrome_page_obj.dismiss_message_box_if_any()
     context.chrome_page_obj.check_document_ready_state(title)
-    assert_that(str(context.chrome_page_obj.check_for_errors_in_chrome_page),
+    assert_that(str(context.chrome_page_obj.check_for_errors_in_chrome_page()),
                 is_not("ERR_TIMED_OUT".lower()),
                 raises(ValueError, " Page Timeout Occured User Plane Error"))
     title = title.lower()
