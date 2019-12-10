0a82ee51887e1941f535ede05afa162a4a37833b
purush <purushflam@gmail.com>
2019-12-10 05:25:32 +0000

    updated

--- chrome/ChromeDriverPage.py
+++ chrome/ChromeDriverPage.py
@@ -86,7 +86,11 @@ class ChromeDriverPage():
             while no_links > 0:
                 try:
                     self.dismiss_message_box_if_any()
-                    self.driver.get(self.html_links.pop(randint(0, (len(self.html_links) - 1))))
+                    if self.html_links.count() > 0:
+                        self.driver.get(self.html_links.pop(randint(0, (len(self.html_links) - 1))))
+                    else:
+                        print("No Links found in Page")
+                        break
                     no_links -= 1
                 except Exception as error:
                     print("Chromedriver exception during click_links_from_page " + str(error))
