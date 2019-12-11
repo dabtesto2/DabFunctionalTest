0baba2bba4dc2a6c32532603a7ec1f4fe3d020bf
purush <purushflam@gmail.com>
2019-12-11 06:28:34 +0000

    updated

--- features/steps/android_test.py
+++ features/steps/android_test.py
@@ -25,10 +25,10 @@ def step_impl(context, status):
     android_device_obj.open_android_device_status_page("About phone")
     android_device_obj.set_android_wait(2)
     network_conn = android_device_obj.get_network_connection()
+    android_device_obj.get_android_device_screen_shot()
     assert_that(network_conn, equal_to_ignoring_whitespace(status),
                 raises(ValueError, "No User plane for this session Mobile returned status = " + network_conn))
     print(android_device_obj.get_device_info())
-    android_device_obj.get_android_device_screen_shot()
     del android_device_obj
 
 
@@ -37,6 +37,7 @@ def step_impl(context, device_profile, network_type):
     android_device_obj = AndroidDevice(device_profile)
     android_device_obj.dismiss_message_box_if_any()
     android_device_obj.start_android_plugin_app()
+    android_device_obj.get_android_device_screen_shot()
     assert_that(android_device_obj.get_data_network_type(), contains_string(network_type), "Network type not matched")
     print("Network Type assigned for the device  "+android_device_obj.get_data_network_type())
     print("Data Connection State  " + android_device_obj.get_data_state())
