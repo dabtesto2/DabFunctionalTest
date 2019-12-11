b01955b1833896705480dd464eb5b0c6e54b0edc
purush <purushflam@gmail.com>
2019-12-11 19:28:44 +0000

    updated

--- features/steps/chrome_test.py
+++ features/steps/chrome_test.py
@@ -12,7 +12,7 @@ def step_impl(context, browser, url):
         chrome_page_obj = ChromeDriverPage(context.device_id)
         chrome_page_obj.dismiss_message_box_if_any()
         chrome_page_obj.get_web_page_using_chrome_browser(url)
-        allure.attach(context.chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_page",
+        allure.attach(chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_page",
                       attachment_type=AttachmentType.PNG)
         context.chrome_page_obj = chrome_page_obj
 
