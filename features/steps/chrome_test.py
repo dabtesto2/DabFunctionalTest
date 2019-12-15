256476d189a4c02c3f52612f1f51d6f915e99872
purush <purushflam@gmail.com>
2019-12-15 07:51:05 +0000

    updated

--- features/steps/chrome_test.py
+++ features/steps/chrome_test.py
@@ -21,19 +21,16 @@ def step_impl(context, browser, url):
 def step_impl(context, title, no_links):
     context.chrome_page_obj.dismiss_message_box_if_any()
     context.chrome_page_obj.check_document_ready_state(title)
-    allure.attach(context.chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_"+title,
-                  attachment_type=AttachmentType.PNG)
     context.chrome_page_obj.save_chrome_web_page_screenshot()
     assert_that(context.chrome_page_obj.check_for_errors_in_chrome_page(),
                 is_not("ERR_TIMED_OUT".lower()),
                 "Chrome Browser Page Timeout Occured when loading URL")
     assert_that(not context.chrome_page_obj.check_for_reload_button_in_chrome_page(),
                 "Chrome Browser Page Timeout Occured with Reload Option")
-    assert_that(context.chrome_page_obj.check_for_errors_in_chrome_page(),
-                is_not("ERR_TIMED_OUT".lower()),
-                "Chrome Browser Page Timeout Occured when loading URL")
     title = title.lower()
     assert_that(context.chrome_page_obj.get_web_page_source(), contains_string(title), raises(ValueError, title))
     context.chrome_page_obj.get_links_from_page()
     context.chrome_page_obj.click_links_from_page(no_links)
+    allure.attach(context.chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_" + title,
+                  attachment_type=AttachmentType.PNG)
     del context.chrome_page_obj
