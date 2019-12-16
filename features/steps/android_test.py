8fea9a856dab585a0164aa2c3f73ca20dc609b76
purush <purushflam@gmail.com>
2019-12-16 23:01:26 +0000

    updated

--- features/steps/android_test.py
+++ features/steps/android_test.py
@@ -19,7 +19,8 @@ def step_impl(context, device_profile, apn_name, seconds):
         android_device_obj.click_android_home()
         del android_device_obj
     else:
-        pass
+        print("skip select APN..")
+
 
 @when(u'Network connection status is "{status}" collect device information')
 def step_impl(context, status):
