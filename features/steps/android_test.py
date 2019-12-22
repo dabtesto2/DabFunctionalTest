dd3dc32e2e500677f747d5549e749a8c0fd8036f
purush <purushflam@gmail.com>
2019-12-22 17:19:25 +0000

    updated

--- features/steps/android_test.py
+++ features/steps/android_test.py
@@ -12,6 +12,7 @@ def step_impl(context, device_profile, apn_name, seconds):
         context.device_profile = device_profile
         android_device_obj = AndroidDevice(device_profile)
         android_device_obj.dismiss_message_box_if_any()
+        android_device_obj.stop_android_plugin_app()
         android_device_obj.select_apn(apn_name)
         android_device_obj.stop_android_plugin_app()
         android_device_obj.start_android_plugin_app()
