d67f9c736c7e74065a428d1311b19ef2ec3f31a1
purush <purushflam@gmail.com>
2020-01-09 22:28:16 +0000

    updated

--- features/steps/webpage.py
+++ features/steps/webpage.py
@@ -13,3 +13,4 @@ def step_impl(context, url):
     allure.attach(chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_page",
                   attachment_type=AttachmentType.PNG)
     context.chrome_page_obj = chrome_page_obj
+    del context.chrome_page_obj
