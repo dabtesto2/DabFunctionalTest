9ca747fb18dea32e17c5611ffbaf30001553850d
purush <purushflam@gmail.com>
2020-01-09 22:35:13 +0000

    updated

--- features/steps/webpage.py
+++ features/steps/webpage.py
@@ -16,17 +16,9 @@ def step_impl(context, url):
 
 
 @then(u'Check if page loads with "{title}" and click "{no_links}" links')
-def step_impl(context, title, no_links, error):
+def step_impl(context, title, no_links):
     context.chrome_page_obj.dismiss_message_box_if_any()
     context.chrome_page_obj.check_document_ready_state(title)
-    if_error_page_timeout = context.chrome_page_obj.check_for_chrome_page_timeout()
-    if_page_contain_reload_btn = context.chrome_page_obj.check_for_reload_button_in_chrome_page()
-    assert_that(if_error_page_timeout, is_not(error.lower()),
-                "Chrome Browser Page Timeout Occured when loading URL")
-    assert_that(not if_page_contain_reload_btn,
-                "Chrome Browser Page Timeout Occured with Reload Option")
-    title = title.lower()
-    assert_that(context.chrome_page_obj.get_web_page_source(), contains_string(title), raises(ValueError, title))
     context.chrome_page_obj.get_links_from_page()
     context.chrome_page_obj.click_links_from_page(no_links)
     allure.attach(context.chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_" + title,
