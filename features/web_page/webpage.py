c0fbd0cf7dbeae9340857c34502b272e6bfc0bcc
purush <purushflam@gmail.com>
2020-01-09 21:41:06 +0000

    updated

---
+++ features/web_page/webpage.py
@@ -0,0 +1,16 @@
+import allure
+from allure_commons.types import AttachmentType
+from behave import then
+
+from chrome.ChromeDriverPage import ChromeDriverPage
+
+
+@then(u'Open chrome browser and get url "{url}"')
+def step_impl(context, browser, url):
+    if ("chrome" or "Chrome" or "CHROME") in browser:
+        chrome_page_obj = ChromeDriverPage(context.device_id)
+        chrome_page_obj.dismiss_message_box_if_any()
+        chrome_page_obj.get_web_page_using_chrome_browser(url)
+        allure.attach(chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_page",
+                      attachment_type=AttachmentType.PNG)
+        context.chrome_page_obj = chrome_page_obj
