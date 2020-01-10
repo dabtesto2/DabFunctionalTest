55937bdd9841c8957a40360b9beccb160c8930c1
purush <purushflam@gmail.com>
2020-01-10 16:40:00 +0000

    updated

--- features/steps/webpage.py
+++ features/steps/webpage.py
@@ -4,9 +4,9 @@ from behave import then, Given
 from chrome.ChromeDriverPage import ChromeDriverPage
 
 
-@Given(u'Open chrome browser and get url "{url}"')
-def step_impl(context, url):
-    context.url = url
+@Given(u'Open chrome browser and get url')
+def step_impl(context):
+    url = "https://www.grunenthal.com"
     chrome_page_obj = ChromeDriverPage("ce071717ab7bd73901")
     chrome_page_obj.get_web_page_using_chrome_browser(url)
     context.chrome_page_obj = chrome_page_obj
