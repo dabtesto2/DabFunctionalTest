abe38e3131b8c87933ba1004483fd07b9e53988b
purush <purushflam@gmail.com>
2020-01-10 13:20:24 +0000

    updated

--- features/environment.py
+++ features/environment.py
@@ -7,3 +7,9 @@ def after_step(context, step):
         step_str = step.name
         allure.attach(context.chrome_page_obj.save_chrome_web_page_screenshot(), name=step_str,
                       attachment_type=AttachmentType.PNG)
+
+
+def before_feature(context, feature):
+    if 'web_page_setup' in feature.tags:
+        context.excute_steps(u'Open chrome browser and get url "https://www.grunenthal.com"')
+
