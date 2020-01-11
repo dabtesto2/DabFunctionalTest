32cdf62e7eac4d72e89f636e4108ab4d208f00cb
purush <purushflam@gmail.com>
2020-01-11 22:22:50 +0000

    updated

--- features/steps/webpage.py
+++ features/steps/webpage.py
@@ -23,8 +23,9 @@ def step_impl(context):
 def step_impl(context, link):
     context.chrome_page_obj.check_document_ready_state(context.url)
     context.chrome_page_obj.click_link_on_page(link)
-    window_size = context.chrome_page_obj.get_window_size()
-    startx = int(window_size['width']/2)
     scroll_list = list(map(lambda x: int(x * (window_size['height'] / 4)), [1.5, 2, 2.5, 3, 3.5, 4]))
     for height in scroll_list:
-        print(startx,height)
+        window_size = context.chrome_page_obj.get_window_size()
+        startx = int(window_size['width'] / 2)
+        starty = int(window_size['height'] * 0.80)
+        print(startx,starty,startx,height)
