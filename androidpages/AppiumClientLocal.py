474747476f46b4056b090a06f85397e22eeb4a82
purush <purushflam@gmail.com>
2020-01-13 22:44:49 +0000

    updated

--- androidpages/AppiumClientLocal.py
+++ androidpages/AppiumClientLocal.py
@@ -1,7 +1,6 @@
 class AppiumClientLocal:
     def __init__(self):
         self._url = "http://77.102.130.243:4723/wd/hub"
-        #82.13.42.180
         self.name = "UiAutomator2"
         self.appium_path = "/usr/local/lib/node_modules/appium/build/lib/main.js"
         self.node_path = "/usr/local/bin/node"
