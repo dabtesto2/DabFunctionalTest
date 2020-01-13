d268ab2a2626d9b89cd40860beab13ae147ceaa7
purush <purushflam@gmail.com>
2020-01-13 23:21:27 +0000

    updated

---
+++ iospages/AppiumClientLocal.py
@@ -0,0 +1,11 @@
+class AppiumClientLocal:
+
+    def __init__(self):
+        self._url = "http://77.102.130.243:4723/wd/hub"
+        self.name = "XCUITest"
+
+    def get_remote_url(self):
+        return self._url
+
+    def get_ios_client_name(self):
+        return self.name
