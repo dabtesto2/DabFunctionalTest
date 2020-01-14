975438cfbeb257bddf49f2f88245b9341f694a02
purush <purushflam@gmail.com>
2020-01-14 05:35:48 +0000

    updated

---
+++ safari/SafariDriver.py
@@ -0,0 +1,19 @@
+from iospages.AppiumClientLocal import AppiumClientLocal
+from appium import webdriver
+
+
+class SafariPage:
+    def __init__(self, device_profile, timeout=10):
+        self.safari_driver_timeout = timeout
+        self.html_links = []
+        self.platform = "iOS"
+        self.scroll = 10
+        self.browser = "Safari"
+        self.device_id = device_profile
+        client = AppiumClientLocal()
+        desired_caps = dict(automationName=client.name, platformName=self.platform, browserName=self.browser,
+                            udid=self.device_id, deviceName=self.platform
+                            )
+        self.driver = webdriver.Remote(client.get_remote_url(), desired_caps)
+        self.driver.implicitly_wait(self.safari_driver_timeout)
+
