42597051dfb2d21a933f43dd268100e5e047e792
purush <purushflam@gmail.com>
2020-01-14 06:08:20 +0000

    updated

--- features/steps/webpage.py
+++ features/steps/webpage.py
@@ -18,17 +18,22 @@ def step_impl(context, url, device):
 def step_impl(context, url, device):
     context.url = url
     safari_page_obj = SafariPage(device)
-    #safari_page_obj.get_web_page_using_safari_browser(url)
+    # safari_page_obj.get_web_page_using_safari_browser(url)
     # safari_page_obj.safari_find_element_containing_text_and_click("Ok, continue to the website")
     context.safari_page_obj = safari_page_obj
 
 
-@then(u'print links from page')
+@then(u'print links from safari page')
 def step_impl(context):
-    context.chrome_page_obj.print_links_from_page()
+    pass
 
 
-@then(u'Check if page loads with "{link}"')
+@then(u'print links from chrome page')
+def step_impl(context):
+    context.chrome_page_obj.print_links_from_chrome_page()
+
+
+@then(u'Check if chrome page loads with "{link}"')
 def step_impl(context, link):
     context.chrome_page_obj.check_document_ready_state(context.url)
     context.chrome_page_obj.click_link_on_page(link)
@@ -40,3 +45,8 @@ def step_impl(context, link):
         context.chrome_page_obj.chrome_scroll_to_page(x_position, y_position)
         allure.attach(context.chrome_page_obj.save_chrome_web_page_screenshot(), name=file,
                       attachment_type=AttachmentType.PNG)
+
+
+@then(u'Check if chrome page loads with "{link}"')
+def step_impl(context, link):
+    pass
