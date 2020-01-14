e847023fa41ef21c7979e36bad3c8983b9922ccc
purush <purushflam@gmail.com>
2020-01-14 20:09:29 +0000

    updated

--- features/steps/webpage.py
+++ features/steps/webpage.py
@@ -19,14 +19,15 @@ def step_impl(context, url, device):
     context.url = url
     safari_page_obj = SafariPage(device)
     safari_page_obj.get_web_page_using_safari_browser(url)
-    safari_page_obj.safari_find_element_containing_text_and_click("Ok, continue to the website")
+   # safari_page_obj.safari_find_element_containing_text_and_click("Ok, continue to the website")
     context.safari_page_obj = safari_page_obj
+    del context.safari_page_obj
 
 
 @then(u'print links from safari page')
 def step_impl(context):
-    context.safari_page_obj.print_links_from_safari_page()
-    del context.safari_page_obj
+    pass
+    #context.safari_page_obj.print_links_from_safari_page()
 
 
 @then(u'print links from chrome page')
