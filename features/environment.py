7c976ba7995e05726b5ce088c204be358f905c40
purush <purushflam@gmail.com>
2020-01-16 06:45:35 +0000

    updated

--- features/environment.py
+++ features/environment.py
@@ -3,11 +3,15 @@ from allure_commons.types import AttachmentType
 
 
 def before_feature(context, feature):
-    if 'web_page_chrome_setup' in feature.tags:
+    if 'android_s8_web_page_chrome_setup' in feature.tags:
         context.execute_steps('''
                                 Given Open chrome browser get url "https://www.grunenthal.com" using "ce071717ab7bd73901"
                                 ''')
-    if 'web_page_safari_setup' in feature.tags:
+    if 'ios_iphone8_web_page_safari_setup' in feature.tags:
+        context.execute_steps('''
+                                Given Open safari browser get url "https://www.grunenthal.com" using "8d0a9f6c8218eaa1356fdf3e4e08573d1228ae3a"
+                                ''')
+    if 'ios_ipad_mini_web_page_safari_setup' in feature.tags:
         context.execute_steps('''
                                 Given Open safari browser get url "https://www.grunenthal.com" using "caecc423e608824f597d8a3dbc382b3addc0382f"
                                 ''')
--- features/web_page/android_s8_browse_webpage_links.feature
+++ features/web_page/android_s8_browse_webpage_links.feature
@@ -1,4 +1,4 @@
-  @web_page_chrome_setup
+  @android_s8_web_page_chrome_setup
   Feature: Get links from web page
   Scenario Outline: Open page https://www.grunenthal.com using chrome browser
      Then Check if chrome page loads with "<link>"
