197ddcde8dbf2a9f6f693c558a7e3184d1be3f2b
purush <purushflam@gmail.com>
2020-01-17 09:27:19 +0000

    updated

--- safari/SafariDriver.py
+++ safari/SafariDriver.py
@@ -29,8 +29,8 @@ class SafariPage:
 
     def safari_find_element_containing_link_and_click(self, search_string):
         try:
-            element = self.driver.find_elements_by_xpath("//XCUIElementTypeLink[@label='"+search_string+"']")
-            #element = self.driver.find_elements_by_xpath("//*[contains(text(), '" + search_string + "')]")
+            #element = self.driver.find_elements_by_xpath("//XCUIElementTypeLink[@label='"+search_string+"']")
+            element = self.driver.find_elements_by_xpath("//*[contains(text(), '" + search_string + "')]")
             for elem in element:
                 elem.click()
         except Exception as error:
