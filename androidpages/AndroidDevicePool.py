1e15f2d067e91d693fbbf16fe3bdcca4f5643f48
purush <purushflam@gmail.com>
2020-01-20 21:21:05 +0000

    updated

--- androidpages/AndroidDevicePool.py
+++ androidpages/AndroidDevicePool.py
@@ -3,9 +3,9 @@ class AndroidDevicePool():
     def __init__(self, device_profile):
         self.device_profile = device_profile
         self._device = {
-            "dev1": {"udid": "ce0417145b24bca70d", "profile": "NDS", "msisdn": "447516903691", "imsi":"234106663078773"},
-            "dev2": {"udid": "2adbc704fc0d7ece", "profile": "PC", "msisdn":"447725629466", "imsi":"234106663078775"},
-            "dev3": {"udid": "ce071717ab7bd73901", "profile": "OA",  "msisdn": "447517999432", "imsi":"234101333546803"}
+            "dev1": {"udid": "ce0417145b24bca70d", "profile": "NDS", "msisdn": "447516903691", "imsi":"234106663078773","model":"androids8"},
+            "dev2": {"udid": "2adbc704fc0d7ece", "profile": "PC", "msisdn":"447725629466", "imsi":"234106663078775","model":"Galaxy"},
+            "dev3": {"udid": "ce071717ab7bd73901", "profile": "OA",  "msisdn": "447517999432", "imsi":"234101333546803","model":"s8"}
         }
         self.platform = "Android"
         self.fastReset = True
@@ -31,3 +31,9 @@ class AndroidDevicePool():
             if device['profile'] in self.device_profile:
                 return device['imsi']
         raise Exception(f'Requested device profile {self.device_profile} not Found')
+
+    def get_android_device_id_for_model(self):
+        for device in self._device.values():
+            if device['model'] in self.device_profile:
+                return device['udid']
+        raise Exception(f'Requested device model {self.device_profile} not Found')
