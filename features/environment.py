0c071525b20b3ee1bc30ab2be0c5b43334d2af60
purush <purushflam@gmail.com>
2020-01-20 21:55:56 +0000

    updated

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ from allure_commons.types import AttachmentType
 def before_feature(context, feature):
     if 'android_s8_web_page_chrome_setup' in feature.tags:
         context.execute_steps('''
-                                Given Open chrome browser get url "https://www.grunenthal.com" using "ce071717ab7bd73901"
+                                Given Open chrome browser get url "https://www.grunenthal.com" using "s8"
                                 ''')
     if 'ios_iphone8_web_page_safari_setup' in feature.tags:
         context.execute_steps('''
