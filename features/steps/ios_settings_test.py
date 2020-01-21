0cf3fdf162eea6a6c7b3fad16990eef0177a599f
purush <purushflam@gmail.com>
2020-01-21 21:51:06 +0000

    updated

--- features/steps/ios_settings_test.py
+++ features/steps/ios_settings_test.py
@@ -1,9 +1,12 @@
 from behave import Given
 
 from iospages.iOSDevicePool import iPhoneDevicePool
+from iospages.iossettings.iosSettings import iosSettings
 
 
 @Given(u'Launch ios Settings for "{model}"')
 def step_impl(context, model):
     ios_obj = iPhoneDevicePool(model)
-    print("value " + ios_obj.get_ios_device_id())
+    print("Device ID " + ios_obj.get_ios_device_id())
+    settings_obj = iosSettings(ios_obj)
+
