ff900a33d959d3728d9ac45de500fead79ec643d
purush <purushflam@gmail.com>
2020-01-21 21:42:47 +0000

    updated

---
+++ features/steps/ios_settings_test.py
@@ -0,0 +1,8 @@
+from behave import Given
+
+from iospages.iOSDevicePool import iPhoneDevicePool
+
+
+@Given(u'Launch ios Settings for "{model}"')
+def step_impl(context, model):
+    ios_obj = iPhoneDevicePool(model)
