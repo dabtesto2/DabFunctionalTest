694ec6cac8d4d68f7bbac273e2d62cce5fbc6692
purush <purushflam@gmail.com>
2020-01-22 23:00:58 +0000

    updated

--- iospages/iPhoneDevicePool.py
+++ iospages/iPhoneDevicePool.py
@@ -4,8 +4,8 @@ class iPhoneDevicePool():
         self.device_model = device_model
         self._device = {
             "dev1": {"udid": "8d0a9f6c8218eaa1356fdf3e4e08573d1228ae3a", "model": "iphone8",
-                     "xcodesigningid": "test.tangent90.com", "xcodeorgid":"374Q29PQSM","version":"13"}
-         }
+                     "xcodesigningid": "test.tangent90.com", "xcodeorgid": "374Q29PQSM", "version": "13"}
+        }
 
     def get_ios_device_id(self):
         for device in self._device.values():
@@ -30,3 +30,6 @@ class iPhoneDevicePool():
             if device['model'] in self.device_model:
                 return device['version']
         raise Exception(f'Requested device model {self.device_model} not Found')
+
+    def get_ios_current_model(self):
+        return self.device_model
