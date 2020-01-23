1a8eea3b9cdeb84d352a7aab3230964d3b58a029
purush <purushflam@gmail.com>
2020-01-23 21:48:14 +0000

    updated

--- features/steps/ios_settings_test.py
+++ features/steps/ios_settings_test.py
@@ -4,9 +4,10 @@ from iospages.iOSDevice import iOSDevice
 from iospages.iPhoneDevicePool import iPhoneDevicePool
 from iospages.iossettings.iosSettings import iosSettings
 
+
 @Given(u'Launch ios Settings for "{model}" with "{apn}" "{username}" "{password}" "{time_wait}"')
 def step_impl(context, model, apn, username, password, time_wait):
     iphone_obj = iPhoneDevicePool(model)
     settings_obj = iosSettings(iphone_obj)
-    settings_obj.switch_airplane_mode()
-    #settings_obj.set_apn(apn, username, password, time_wait)
+    settings_obj.switch_airplane_mode("on")
+    # settings_obj.set_apn(apn, username, password, time_wait)
