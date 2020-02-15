a9a8463ff3710461b233f3de86adca34b86afb64
purush <purushflam@gmail.com>
2020-02-15 23:08:59 +0000

    updated

--- features/steps/ios_settings_test.py
+++ features/steps/ios_settings_test.py
@@ -13,11 +13,11 @@ def step_impl(context, model, apn, username, password, time_wait):
     settings_obj = iosSettings(iphone_obj)
     settings_obj.switch_airplane_mode("off")
     settings_obj.set_apn(apn, username, password, time_wait)
-    allure.attach(context.safari_page_obj.save_safari_web_page_screenshot(), name="New PDP context",
+    allure.attach(settings_obj.save_settings_page_screenshot(), name="New PDP context",
                   attachment_type=AttachmentType.PNG)
     settings_obj.switch_airplane_mode("on")
     settings_obj.switch_airplane_mode("off")
     settings_obj.set_iOS_wait(7)
-    allure.attach(context.safari_page_obj.save_safari_web_page_screenshot(), name="Mobile Data status",
+    allure.attach(settings_obj.save_settings_page_screenshot(), name="Mobile Data status",
                   attachment_type=AttachmentType.PNG)
 
