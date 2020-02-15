4b8fbe08836ad0e44feb81f4423c316b19b8e2b8
purush <purushflam@gmail.com>
2020-02-15 12:48:08 +0000

    updated

--- features/steps/ios_settings_test.py
+++ features/steps/ios_settings_test.py
@@ -13,5 +13,4 @@ def step_impl(context, model, apn, username, password, time_wait):
     settings_obj.set_apn(apn, username, password, time_wait)
     settings_obj.switch_airplane_mode("on")
     settings_obj.switch_airplane_mode("off")
-    del settings_obj
 
