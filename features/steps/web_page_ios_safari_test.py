c8c3fc8b7580250110c7815b5db3346854dc01e6
purush <purushflam@gmail.com>
2020-02-16 13:52:21 +0000

    updated

--- features/steps/web_page_ios_safari_test.py
+++ features/steps/web_page_ios_safari_test.py
@@ -13,7 +13,7 @@ def step_impl(context, url, model):
     safari_page_obj = SafariPage(ios_obj)
     safari_page_obj.get_web_page_using_safari_browser(url)
     safari_page_obj.check_safari_document_ready_state(context.url)
-    safari_page_obj.safari_find_element_containing_link_and_click("Ok, continue to the website")
+    #safari_page_obj.safari_find_element_containing_link_and_click("Ok, continue to the website")
     context.safari_page_obj = safari_page_obj
     allure.attach(safari_page_obj.save_safari_web_page_screenshot(), name="Web Page",
                   attachment_type=AttachmentType.PNG)
