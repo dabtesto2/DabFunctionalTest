9a643ad7a45d0b93336131be9591b0c7e3611555
purush <purushflam@gmail.com>
2020-02-16 08:29:39 +0000

    updated

--- features/steps/ios_settings_test.py
+++ features/steps/ios_settings_test.py
@@ -15,5 +15,8 @@ def step_impl(context, model, apn, username, password, time_wait):
     settings_obj.set_apn(apn, username, password, time_wait)
     settings_obj.switch_airplane_mode("on")
     settings_obj.switch_airplane_mode("off")
-    settings_obj.set_iOS_wait(7)
+    settings_obj.set_iOS_wait(2)
+    allure.attach(settings_obj.save_settings_page_screenshot(), name="Mobile Data Connection",
+                  attachment_type=AttachmentType.PNG)
+    
 
