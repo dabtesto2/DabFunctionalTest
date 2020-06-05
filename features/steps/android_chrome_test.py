3db2af5ca80ee1e8f09ca6ee3273e5b7c76e9606
purush <purushflam@gmail.com>
2020-06-05 22:14:38 +0100

    updated

--- features/steps/android_chrome_test.py
+++ features/steps/android_chrome_test.py
@@ -3,7 +3,21 @@ from allure_commons.types import AttachmentType
 from behave import *
 from androidpages.chrome.ChromeDriverPage import ChromeDriverPage
 from hamcrest import *
+import time
 
+
+# added on 05/06/2020
+@then(u'Open "{browser}" browser and get url "{url}" and wait "{seconds}"')
+def step_impl(context, browser, url, seconds):
+    if ("chrome" or "Chrome" or "CHROME") in browser:
+        chrome_page_obj = ChromeDriverPage(context.device_id)
+        chrome_page_obj.dismiss_message_box_if_any()
+        chrome_page_obj.get_web_page_using_chrome_browser(url)
+        allure.attach(chrome_page_obj.save_chrome_web_page_screenshot(), name="Chrome_page",
+                      attachment_type=AttachmentType.PNG)
+        context.chrome_page_obj = chrome_page_obj
+        time.sleep(seconds)
+# added on 05/06/2020
 
 @then(u'Open "{browser}" browser and get url "{url}"')
 def step_impl(context, browser, url):
