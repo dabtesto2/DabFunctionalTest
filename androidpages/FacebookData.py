b43689530e973ca97a6cf999352aac30dda39fcb
purush <purushflam@gmail.com>
2020-06-07 18:55:58 +0100

    updated

--- androidpages/FacebookData.py
+++ androidpages/FacebookData.py
@@ -12,7 +12,7 @@ class FacebookData:
 
     def get_user_videos(self):
         video_entries = []
-        print(" Token is {token}".format(self.token))
+        print(" Token is {}".format(self.token))
         graph = facebook.GraphAPI(self.token)
         user_video_obj = graph.get_object('me/videos/uploaded/', fields='source')
         user_videos = json.dumps(user_video_obj, indent=4)
