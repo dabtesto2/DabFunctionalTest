9a0e9dd3f98d4fa8c30ae7fabef4cb775d07c976
purush <purushflam@gmail.com>
2020-06-08 12:15:02 +0100

    updated

--- features/steps/android_chrome_test.py
+++ features/steps/android_chrome_test.py
@@ -20,7 +20,7 @@ def step_impl(context, browser, url, user, password, token, seconds):
         for v_data_url in video_entries:
             print("****** video url {}".format(v_data_url))
             chrome_page_obj_v = ChromeDriverPage(context.device_id)
-            chrome_page_obj_v.chrome_fb_login(url,user,password,seconds)
+            #chrome_page_obj_v.chrome_fb_login(url,user,password,seconds)
             chrome_page_obj_v.get_web_page_using_chrome_browser(v_data_url)
             chrome_page_obj_v.chrome_click_play()
             time.sleep(int(seconds))
@@ -28,7 +28,7 @@ def step_impl(context, browser, url, user, password, token, seconds):
         for p_data_url in photo_entries:
             print("****** photo url {}".format(p_data_url))
             chrome_page_obj_p = ChromeDriverPage(context.device_id)
-            chrome_page_obj_p.chrome_fb_login(url,user,password,seconds)
+            #chrome_page_obj_p.chrome_fb_login(url,user,password,seconds)
             chrome_page_obj_p.get_web_page_using_chrome_browser(p_data_url)
             chrome_page_obj_v.chrome_click_play()
             time.sleep(int(seconds))
