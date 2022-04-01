6203b6bdf9a8130cd7f9fef7308b75424926f2e4
purush <dabtesto2@gmai.com>
2022-04-01 17:39:10 +0100

    updated

--- androidpages/FacebookData.py
+++ androidpages/FacebookData.py
@@ -1,4 +1,4 @@
-import facebook
+from pyfacebook import GraphAPI
 import json
 
 token = "EAAhW5zZBlA3IBAGzAevjtREZBZC3PoAsNw7b50wqxZA7ZC4CPNQXGYIJ1MckvZB2LLOiVf2BX5HZBU3UHVuFGilJJutOd0GS8MZAQPBeQjdwsYqVMdFCQv3Ez8MolQIWtv9r1s4J7JysnowfjJc51rIBhDvSpT7f2YPY8OYGmgCft4BJMaiAe51P"
@@ -12,7 +12,7 @@ class FacebookData:
 
     def get_user_videos(self):
         video_entries = []
-        graph = facebook.GraphAPI(self.token)
+        graph = GraphAPI(self.token)
         user_video_obj = graph.get_object('me/videos/uploaded/', fields='source')
         user_videos = json.dumps(user_video_obj, indent=4)
         video_data = json.loads(user_videos)
@@ -28,7 +28,7 @@ class FacebookData:
 
     def get_user_photos(self):
         photo_entries = []
-        graph = facebook.GraphAPI(self.token)
+        graph = GraphAPI(self.token)
         user_photos_obj = graph.get_object('me/photos/uploaded/', fields='picture')
         user_photos = json.dumps(user_photos_obj, indent=4)
         photo_data = json.loads(user_photos)
