82ceb697cc6a62533e2a94167645c0698f72b24f
purush <dabtesto2@gmai.com>
2022-04-02 11:14:26 +0100

    updated

--- androidpages/AppiumClientLocal.py
+++ androidpages/AppiumClientLocal.py
@@ -1,6 +1,6 @@
 class AppiumClientLocal:
     def __init__(self):
-        self._url = "http://77.102.130.243:4723/wd/hub"
+        self._url = "http://86.21.181.20:4723/wd/hub"
         self.name = "UiAutomator2"
 
     def get_remote_url(self):
