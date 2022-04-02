874d7f8316b54404853875c41f9fc7b7f120cbd4
purush <dabtesto2@gmai.com>
2022-04-02 20:18:42 +0100

    new

--- androidpages/chrome/ChromeDriverPage.py
+++ androidpages/chrome/ChromeDriverPage.py
@@ -21,7 +21,7 @@ class ChromeDriverPage:
         self.browser = "Chrome"
         self.device_id = device_id
         self.chromedriverExecutable = "/Users/purushothamanloganathan/chromedriver/"
-        self.chromedriverVersion = 101
+        self.chromedriverVersion = 99
         self.chromedriverpath = self.chromedriverExecutable + str(self.chromedriverVersion) + "/chromedriver"
         client = AppiumClientLocal()
         options = ChromeOptions()
